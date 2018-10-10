60bc17d98aa4c30d40d3130bb2e1ce52148a71a2
Marc Rodriguez <mrodriguez@netquest.com>
2018-10-10 16:09:49 +0200

    Implement playMove

--- reducers.js
+++ reducers.js
@@ -5,10 +5,7 @@ const R = require('ramda')
 
 
 const initialTable = {
-    holes: {
-        player: [4, 4, 4, 4, 4, 4],
-        opponent: [4, 4, 4, 4, 4, 4],
-    },
+    holes: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
     deposit: {
         player: 0,
         opponent: 0,
@@ -18,10 +15,10 @@ const initialTable = {
 
 const NUMBER_OF_HOLES = 12
 
-const sowHole = (sowedSeeds, reapedHole, plantedSeeds, hole) => {
+const sowHole = (reapedSeeds, reapedHole, plantedSeeds, hole) => {
     if (hole === reapedHole) return 0
-    const seedsPerHole = Math.floor(sowedSeeds / (NUMBER_OF_HOLES - 1))
-    const restOfSeeds = sowedSeeds % (NUMBER_OF_HOLES - 1)
+    const seedsPerHole = Math.floor(reapedSeeds / (NUMBER_OF_HOLES - 1))
+    const restOfSeeds = reapedSeeds % (NUMBER_OF_HOLES - 1)
     const distanceToSowedHole = reapedHole < hole
         ? hole - reapedHole
         : (NUMBER_OF_HOLES - reapedHole) + hole
@@ -32,24 +29,24 @@ const sowHole = (sowedSeeds, reapedHole, plantedSeeds, hole) => {
     return remainingSeeds
 }
 
-const sow = (reapedHole, table) => table.map(
-    (plantedSeeds, hole) => sowHole(table[reapedHole], reapedHole, plantedSeeds, hole)
-)
+const sow = R.curry((reapedHole, tableHoles) => tableHoles.map(
+    (plantedSeeds, hole) => sowHole(tableHoles[reapedHole], reapedHole, plantedSeeds, hole)
+))
 
 const hasTwoOrThree = x => x === 2 || x === 3
 
-const reapRemainsFrom = (lastSowedHole, table, reapedRemains) => {
+const reapRemainsFrom = (lastSowedHole, tableHoles, reapedRemains) => {
     const nextHole = (lastSowedHole + 1) % NUMBER_OF_HOLES
-    if (!hasTwoOrThree(table[nextHole])) return {table, reapedRemains}
-    const newTable = R.assoc(nextHole, 0, table)
+    if (!hasTwoOrThree(tableHoles[nextHole])) return {tableHoles, reapedRemains}
+    const newTable = R.assoc(nextHole, 0, tableHoles)
     return reapedRemainsFrom(nextHole, newTable, reapedRemains + table[nextHole])
 }
 
-const reapRemains = (sowedSeeds, reapedHole, table) => {
+const reapRemains = R.curry((sowedSeeds, reapedHole, tableHoles) => {
     const restOfSeeds = sowedSeeds % (NUMBER_OF_HOLES - 1)
     const lastSowedHole = (reapedHole + restOfSeeds) % NUMBER_OF_HOLES
-    return reapRemainsFrom(lastSowedHole, table, 0)
-}
+    return reapRemainsFrom(lastSowedHole, tableHoles, 0)
+})
 
 const fieldOfPlayer = (player, table) => {
     const tableMiddle = Math.ceil(NUMBER_OF_HOLES / 2)
@@ -67,21 +64,17 @@ const isInvalidTable = (player, table) => {
     return isFieldEmpty
 }
 
-const reapFrom = (player, hole, table) => {
-    const newTable = {
-        ...table,
-        holes: {
-            ...table.holes,
-            [player]: table.holes[player].map((seeds, holeNum) => holeNum === hole ? 0 : seeds)
-        },
-    }
-    const reapedSeeds = table.holes[player][hole]
-    return {newTable, reapedSeeds}
-}
+const storeRemains = (player, remains, table) => ({
+    ...table,
+    deposit: R.assoc(player, table.deposit[player] + remains, table.deposit)
+})
 
-const plantSeedsFrom = (player, hole, table, seeds) => {
-
-}
+const playMove = (player, hole, table) => R.pipe(
+    sow(hole),
+    reapRemains(table.holes[hole], hole),
+    ({tableHoles, reapedRemains}) => ({table: {...table, holes: tableHoles}, reapedRemains}),
+    ({table: newTable, reapedRemains}) => storeRemains(player, reapedRemains, newTable),
+)(table.holes)
 
 const table = reducerHash({
     [PLAYER_REAPS]: (state, {value: hole}) => reapFrom('player', hole, state).newTable,
